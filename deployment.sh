1532845ae5dbee99833cd8c9e93cb7d49ccf6bd0
PhamHaiLong99 <phlexile@gmail.com>
2023-02-15 09:18:18 +0000

    add

--- deployment.sh
+++ deployment.sh
@@ -2,8 +2,8 @@
 docker build -t ubuntu-test .
 docker tag ubuntu-test:latest reg.longph.works/ubuntu-test
 docker push reg.longph.works/ubuntu-test
-
+echo "Going to sleep for waiting..."
 sleep 2
 
-
+echo "Deploy K8s"
 kubectl apply -k .
